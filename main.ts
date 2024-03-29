a12b8934f3e617ddab6a5d1ef8f7ddff6b31421f
Abubaker Siddiqui <162192538+AbubakerSiddiqui001@users.noreply.github.com>
2024-03-30 01:17:45 +0500

    code-day-06

---
+++ main.ts
@@ -0,0 +1,31 @@
+let freinds: string[] = ["Alice", "Bob", "Charlie"];
+
+// Arrays are zero-indexed, so index 3 is out of bounds.
+console.log(freinds[3]);
+// Correcting the error by accessing a valid index.
+freinds[2] = "Charlie"; 
+
+
+// Checked True OR False
+let car = "Saburu";
+console.log(car == "Saburu");  //true
+console.log(car == "Mercides");  //False
+
+
+//Equality with string
+console.log("apple"=="apple");
+console.log("apple"=="apple");
+
+// Lowercase function
+console.log("Apple".toLowerCase()=="apple");
+
+// Numerical test
+console.log(10>5);  //True
+console.log(1>2);  //False
+
+//AND and OR Operators
+console.log(true && false); //False
+console.log(true,false);  //True
+
+// !True cnvrt into false , False cnvrt into True
+console.log(!(10<5));
